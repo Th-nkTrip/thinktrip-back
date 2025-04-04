8395f8c7b11a5ad652e8783bba55b73a7ce2ae15
김정현 <richard.kim146@gmail.com>
2025-04-04 15:45:27 +0900

    initial

---
+++ src/main/java/com/thinktrip/thinktrip_api/ThinktripApiApplication.java
@@ -0,0 +1,13 @@
+package com.thinktrip.thinktrip_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinktripApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThinktripApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thinktrip/thinktrip_api/ThinktripApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinktrip.thinktrip_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinktripApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
