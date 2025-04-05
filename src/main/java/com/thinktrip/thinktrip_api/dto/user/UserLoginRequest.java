6a6c7060dd3dc19684223a961dbb11ce41ab7ed4
김정현 <richard.kim146@gmail.com>
2025-04-05 19:01:03 +0900

    user signup init

---
+++ src/main/java/com/thinktrip/thinktrip_api/dto/user/UserLoginRequest.java
@@ -0,0 +1,14 @@
+// com.thinktrip.thinktrip_api.dto.user.UserLoginRequest.java
+package com.thinktrip.thinktrip_api.dto.user;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserLoginRequest {
+    private String email;
+    private String password;
+}
